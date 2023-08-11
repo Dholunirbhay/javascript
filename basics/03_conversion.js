b2969c0b288ba4cecf4de70e262eb8f90fd5e7a4
Dholunirbhay <102872061+Dholunirbhay@users.noreply.github.com>
2023-08-11 07:05:55 +0000

    conversion in js usin datatypes.

---
+++ basics/03_conversion.js
@@ -0,0 +1,30 @@
+let score ="nirbhay";
+console.log(typeof score);
+console.log(typeof(score)); // typeof() function is used to see the datatype of the given variable.
+  
+let value = Number(score); // Number function is used to convert the variable in number data type.
+console.log(typeof value);
+console.log(value);
+
+// by converting in Number
+// "33" => 33
+// "33abc" => NaN (Not a Number)
+
+let loggedin = "n";
+
+let booleanisloggedin = Boolean(loggedin);
+console.log(booleanisloggedin);
+
+// by converting in boolean
+//1=> true , 0 => false
+//"" => false
+// " nasd" => true
+
+let somenum = 564;
+console.log(typeof(somenum));
+let str = String(somenum);
+
+console.log(typeof(str));
+// by converting in string
+//
+

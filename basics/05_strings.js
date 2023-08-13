20c1940d8ea2f2a172710892b2e0ebdafdf70192
Dholunirbhay <102872061+Dholunirbhay@users.noreply.github.com>
2023-08-13 11:25:14 +0000

    string and maths ops

---
+++ basics/05_strings.js
@@ -0,0 +1,30 @@
+const name ="nirbhay";
+const repocount = 60;
+
+//console.log(name + " has "+ repocount + " repository");
+//console.log(`${name} has ${repocount} repository`);
+
+const game = new String('nirbhay-nd-com');
+
+let ans = game.split("-");
+
+//console.log(ans[0]);
+
+//console.log(game.toUpperCase());
+
+//console.log(game.charAt(13));
+
+//console.log(game.indexOf(3));
+
+//let str = game.substring(1,3);
+//console.log(str);
+ 
+//let slid = game.slice(-8,4);
+//console.log(slid);
+
+//let str2 = "  nirbhay  ";
+//console.log(str2);
+//console.log(str2.trim());
+
+
+
